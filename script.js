7b799301a510e26ddf24d1d4da3b09251f43d1a5
kobi <kobicohencom@gmail.com>
2022-10-21 09:37:46 +0300

    delay change and add only once running mechanism

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-const originalText ={value:'שמי אסף סלמון'} 
-const gibrishText ={value:'משי ספה לסימו'} 
-const text = document.querySelector('#text')
 let intervalCounter = 0
+
+const visitedUser = localStorage.getItem('visit')
+
+console.log(visitedUser)
+localStorage.setItem('visit',1)
+
+
+const originalText ={value:'כל כך פשוט לקרוא משפט'} 
+const gibrishText ={value:'לכ כך שפוט קרלו שטמש'} 
+const text = document.querySelector('#text')
+
 text.innerHTML=originalText.value
+const unit = () =>{
 
-const interval = setInterval(()=>{
-    if (intervalCounter>0){
-
-        clearInterval(interval)
-        console.log('clear interval')
-        setTimeout(()=>{
-            console.log('set Timeout')
-            text.innerHTML=originalText.value
-            
-        },4000)
-    }
-    console.log('first interval')
-
-    text.innerHTML=gibrishText.value
-    intervalCounter++
     
-},2000)
-
-
-
+    const interval = setInterval(()=>{
+        if (intervalCounter>0){
+            
+            clearInterval(interval)
+            console.log('clear interval')
+            setTimeout(()=>{
+                console.log('set Timeout')
+                text.innerHTML=originalText.value
+                
+            },10000)
+        }
+        console.log('first interval')
+        
+        text.innerHTML=gibrishText.value
+        intervalCounter++
+        
+    },5000)
+    
+}
+    
+    
+    
+if (!visitedUser){
+    unit()
+}
